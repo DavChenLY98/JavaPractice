985285092e8ae384252bbca2a09afa76f9ff8fbb
Davch <976604851@qq.com>
2023-12-25 22:53:06 +0800

    Dec25

---
+++ com/henry/Dec15/Dec15.java
@@ -0,0 +1,9 @@
+package com.henry.Dec15;
+
+public class Dec15 {
+    public static void main(String[] args) {
+        int[] arr=new int[5];
+        System.out.println(arr.length);
+
+    }
+}
---
+++ com/henry/Dec18/test.java
@@ -0,0 +1,11 @@
+package com.henry.Dec18;
+
+public class test {
+    public static void main(String[] args) {
+        int i=0,j=0,k=0;
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(k);
+
+    }
+}
